73ef75251bf7cf60379e7d54ad7de25d25a6c6fe
Giorgio Colo <gcolomban@gmail.com>
2020-07-03 20:33:16 +0200

    feat: added client jar files for windows and mac

--- src/main/java/it/polimi/ingsw/server/model/god/godPowers/Teleport.java
+++ src/main/java/it/polimi/ingsw/server/model/god/godPowers/Teleport.java
@@ -30,7 +30,7 @@ public class Teleport extends GodPower {
      * @param posYFrom y coordinate of the worker that is going to use the power
      * @param posXTo x coordinate of the tile targeted by the power
      * @param posYTo y coordinate of the tile targeted by the power
-     * @return 1 if the move succedes, -2 if something went wrong 
+     * @return 1 if the move succedes, -2 if something went wrong
      */
     @Override
     public int power(Board board, int posXFrom, int posYFrom, int posXTo, int posYTo) {
